68ed616e16314d00dd62371a6c95341680194959
amatranga <amatranga892@gmail.com>
2017-12-07 13:34:14 -0600

    Implement simple server
    Simple Express server used to serve up static HTML files

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, './public')));
+
+app.listen(PORT, () => {
+  console.log(`RedditReader is running at port ${PORT}`);
+});
