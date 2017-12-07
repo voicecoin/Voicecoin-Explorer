5960534b12620247047a6c58340a284f08767686
amatranga <amatranga892@gmail.com>
2017-12-07 13:32:08 -0600

    Implement webpack.config.js file
    Compiles JSX into JS.
    Output files will be written to public/dist

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './client/src/index.jsx',
+  output: {
+    path: path.join(__dirname, 'public/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/,
+        include: path.join(__dirname, 'client/src'),
+        exclude: ['node_modules'],
+        use: [
+          { loader: 'babel-loader',
+            options: {
+              presets: ['react', 'es2015']
+            }
+          }
+        ]
+      }
+    ]
+  },
+  externals: {
+    'react/addons': true,
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devtool: 'source-map'
+};
+
+module.exports = config;
