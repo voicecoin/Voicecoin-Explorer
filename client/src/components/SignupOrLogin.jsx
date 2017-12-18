9caaf258dc0860862b34e0543b62fb0378bb9450
amatranga <amatranga892@gmail.com>
2017-12-18 13:41:37 -0600

    Signup/Login now encapsulated in parent component
    SignupOrLogin component handles all API calls for both Signup and Login
    Components.

--- client/src/components/SignupOrLogin.jsx
+++ client/src/components/SignupOrLogin.jsx
@@ -1,52 +1,18 @@
-import axios from 'axios';
 import React from 'React';
 import Login from './Login';
 import Signup from './Signup';
 
-class SignupOrLogin extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.createUser = this.createUser.bind(this);
-    this.getToken = this.getToken.bind(this);
+const SignupOrLogin = props => {
+  const location = props.location;
+  if (location === '/signup') {
+    return (
+      <Signup createUser={props.createUser}/>
+    );
   }
-
-  createUser(user) {
-    axios({
-      method: 'POST',
-      url: 'http://api.voicecoin.net/v1/Account',
-      data: {
-        email: user.email,
-        password: user.password
-      }
-    }).then(res => {
-      this.getToken(user);
-    }).catch(err => {
-      console.error(err, 'Error creating user');
-    });
-  }
-
-  getToken(user) {
-    axios({
-      method: 'POST',
-      url: `http://api.voicecoin.net/v1/Account/token?username=${user.email}&password=${user.password}`
-    }).then(res => {
-      console.log(res, 'Result from getToken')
-    }).catch(err => {
-      console.error(err, 'Error getting token');
-    });
-  }
-
-  render() {
-    if (this.props.location.pathname === '/signup') {
-      return (
-        <Signup createUser={this.createUser}/>
-      );
-    } else {
-      return (
-        <Login getToken={this.getToken}/>
-      );
-    }
+  if (location === '/login') {
+    return (
+      <Login getToken={props.getToken}/>
+    );
   }
 }
 
